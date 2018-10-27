b81c026f945c5fd44499ef0af23c21e3b2cef4fd
Salvi Pascual <salvi.pascual@gmail.com>
2018-10-27 12:34:31 -0400

    do not show inactive on the web

--- service.php
+++ service.php
@@ -11,41 +11,39 @@ class Servicios extends Service
 	public function _main($request)
 	{
 		// get list of services
-		$connection = new Connection();
-		$result = $connection->query("SELECT name, description, category FROM service WHERE listed>0");
+		$result = Connection::query("SELECT name, description, category FROM service WHERE listed = 1");
 
 		// get the path to www
 		$di = \Phalcon\DI\FactoryDefault::getDefault();
 		$wwwroot = $di->get('path')['root'];
+		$environment = $di->get('environment');
 
 		// create array of arrays
-		$services = array();
-		$others = array();
-		foreach($result as $res)
-		{
+		$services = [];
+		$others = [];
+		foreach($result as $res) {
 			// get the image of the service
 			$res->image = "$wwwroot/services/{$res->name}/{$res->name}.png";
 			if( ! file_exists($res->image)) $res->image = "$wwwroot/public/images/noicon.png";
 
 			// to keep the categoty "others" at the end
-			if($res->category == "otros")
-			{
+			if($res->category == "otros") {
 				$others[] = $res;
 				continue;
 			}
 
 			// group all other categories in a big array
-			if( ! isset($services[$res->category])) $services[$res->category] = array();
+			if( ! isset($services[$res->category])) $services[$res->category] = [];
 			array_push($services[$res->category], $res);
 		}
 
 		// sort by category alphabetically and merge to "other"
 		ksort($services);
-		$services = array_merge($services, array("otros"=>$others));
+		$services = array_merge($services, ["otros"=>$others]);
 
 		// get variables to send to the template
-		$template = ($di->get('environment') == "email") ? "email.tpl" : "web.tpl";
-		$responseContent = array("services" => $services, "serviceNum" => count($result));
+		$template = $environment=="web" ? "web.tpl" : "email.tpl";
+		$responseContent = ["services" => $services, "serviceNum" => count($result)];
 
 		// create response
 		$response = new Response();
