945b2508f15c20ff86631abe4063977a0f59fbb5
salvipascual <salvi.pascual@gmail.com>
2019-12-11 17:23:12 -0500

    new version to Core3

--- scripts.js
+++ scripts.js
@@ -2,39 +2,10 @@ $(document).ready(function () {
 	$('select').formSelect();
 });
 
-function filter(selectedFilter) {
-	if(selectedFilter == 'todo') selectedFilter = false;
-	$('#services').html(filteredServices(selectedFilter))
-}
-
-function serviceCard(service) {
-	service.name = service.name.charAt(0).toUpperCase() + service.name.slice(1);
-	return "<div class=\"col s4 m2\" title=\"" + service.description + "\">" +
-				"<div class=\"card\" style=\"padding: 1rem\">" +
-					"<a href=\"#\" class=\"green-text apretaste\"" +
-						"onclick=\"apretaste.send({command : '" + service.name + "'})\">" +
-						"<img src=\"" + appImgPath + "/" + service.image + "\" alt=\"" + service.name + "\" width=\"100%\">" +
-						service.name +
-					"</a>" +
-				"</div>" +
-			"</div>"
-}
-
-function filteredServices(filter = false) {
-	var html = "";
-	for (var category in services) {
-		if (filter && category != filter) continue;
-		var name = category.charAt(0).toUpperCase() + category.slice(1);
-
-		html += "<h5 class=\"green-text\"><b>" + name + "</b></h5>" +
-				"<div class=\"row\">";
-
-		services[category].forEach(function (service) {
-			html += serviceCard(service)
-		});
-
-		html += "</div><div class=\"divider\"></div>"
+function filter(category) {
+	if(category == '') $('.service').slideDown();
+	else {
+		$('.service').hide();
+		$('.'+category).slideDown();		
 	}
-
-	return html;
 }
--- service.php
+++ service.php
@@ -1,6 +1,8 @@
 <?php
 
-use Apretaste\Core;
+use Apretaste\Request;
+use Apretaste\Response;
+use Framework\Database;
 
 class Service
 {
@@ -14,44 +16,31 @@ class Service
 	public function _main(Request $request, Response $response)
 	{
 		// get list of services
-		$result = Connection::query("SELECT name, description, category FROM service WHERE listed = 1");
+		$services = Database::query("
+			SELECT name, category 
+			FROM service 
+			WHERE listed = 1
+			ORDER BY name ASC");
 
-		// get the path to www
-		$wwwroot = Core::getRoot();
-		$servicesPath = "$wwwroot/shared/services";
-
-		// create array of arrays
+		// get the image for the service
 		$images = [];
-		$services = [];
-		$others = [];
-		foreach ($result as $res) {
-			// get the image for the service
-			$imgPath = "$servicesPath/{$res->name}/{$res->name}.png";
-			if (!file_exists($imgPath)) $imgPath = "$wwwroot/public/images/noicon.png";
-			$res->image = basename($imgPath);
-
-			// save image to the images array only once
-			if (!in_array($imgPath, $images)) $images[] = $imgPath;
-
-			// to keep the categoty "others" at the end
-			if ($res->category == "otros") {
-				$others[] = $res;
-				continue;
-			}
-
-			// group all other categories in a big array
-			if (!isset($services[$res->category])) $services[$res->category] = [];
-			array_push($services[$res->category], $res);
+		foreach ($services as $r) {
+			$images[] = SERVICE_PATH . $r->name . "/" . $r->name . ".png";
+			$r->name = ucfirst($r->name);
+			$r->category = ucfirst($r->category);
 		}
 
-		// sort by category alphabetically and merge to "other"
-		ksort($services);
-		$services = array_merge($services, ["otros" => $others]);
+		// create the list of categories
+		$categories = [];
+		foreach ($services as $c) {
+			if(!in_array($c->category, $categories)) $categories[] = $c->category;
+		}
 
-		// get variables to send to the template
-		$content = new stdClass();
-		$content->services = $services;
-		$content->serviceNum = count($result);
+		// create the content array
+		$content = [
+			"services" => $services,
+			"categories" => $categories
+		];
 
 		// create response
 		$response->setCache('month');
