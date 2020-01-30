cd2464d7256c2f56d4b6f51863bb7e9ec61c521c
salvipascual <salvi.pascual@gmail.com>
2020-01-30 15:26:10 +0100

    add filter

--- scripts.js
+++ scripts.js
@@ -2,10 +2,47 @@ $(document).ready(function () {
 	$('select').formSelect();
 });
 
-function filter(category) {
-	if(category == '') $('.service').slideDown();
+function options() {
+	$('.drop-down .options').slideDown('fast');
+}
+
+function select(element) {
+	// get text and value from element
+	var text = $(element).find('.text').text();
+	var category = $(element).attr('value');
+
+	// show all categories
+	if(category == 'all') {
+		$('.service').slideDown();
+	} 
+	// filter by category name
 	else {
 		$('.service').hide();
 		$('.'+category).slideDown();		
 	}
+
+	// hide the options bar
+	$('.drop-down .options').hide();
+
+	// add text to the input
+	$('#filter').val(text);
+}
+
+function filter(element) {
+	// get text to filter by
+	var text = $('#filter').val().toLowerCase();
+
+	$('.service').show().each(function(i, e) {
+		// get the caption
+		var caption = $(e).attr('data-value').toLowerCase();
+
+		// hide if caption does not match
+		if(caption.indexOf(text) < 0) {
+			$(e).hide();
+		}
+	})
+}
+
+function clean() {
+	$('#filter').val('');
 }
